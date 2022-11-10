45ccbf9173cacfaff22cf0e71780123b1026a044
Alum-Treutime <paulmmoyers@outlook.com>
2022-11-09 16:51:44 -0900

    Started EDA

--- Data_EDA.py
+++ Data_EDA.py
@@ -7,3 +7,7 @@ Created on Mon Oct 31 13:45:15 2022
 """
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+
+df = pd.read_csv("Data/Clean_Data/OpsCalculations.csv")
+df.head()
