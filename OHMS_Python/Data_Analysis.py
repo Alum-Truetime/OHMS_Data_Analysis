541d223a0701bb97171e013aae7d87ac52a83643
Alum-Treutime <paulmmoyers@outlook.com>
2022-11-28 19:00:13 -0900

    explored the plots

---
+++ OHMS_Python/Data_Analysis.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+#from pandas_profiling import ProfileReport as pp
+
+pd.set_option('display.max_columns', 89)
+
+df = pd.read_csv('Data/CustomerQuotedProduct_clean.csv', encoding='Latin-1')
+
+#profile = ProfileReport(df, title="Pandas Profiling Report")
+
+df.head()
+
+Group = df['fldGroupTypeID']
+Quantity = df['fldQuantity']
+
+plt.bar(Group, Quantity, label='Group Count')
+plt.title('Number of Installers vs Install Hours')
+plt.ylabel('Number of Hours')
+plt.xlabel('Number of Installers')
+plt.show()
