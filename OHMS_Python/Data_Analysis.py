29f2ee95ea1fc70ad86f4397fbfd1c85d4537224
Alum-Treutime <paulmmoyers@outlook.com>
2023-01-11 13:00:45 -0900

    Transfered from Notebook to .py for production

--- OHMS_Python/Data_Analysis.py
+++ OHMS_Python/Data_Analysis.py
@@ -1,18 +1,31 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-#from pandas_profiling import ProfileReport as pp
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+
+plt.style.use('fivethirtyeight')
 
 pd.set_option('display.max_columns', 89)
 
+# Global Variables
+StartDate = '2019-01-01'
+EndDate = '2020-01-01'
+
+# Initial DataFrame of Imported Clean Data
 df = pd.read_csv('Data/CustomerQuotedProduct_clean.csv', encoding='Latin-1')
 
-#profile = ProfileReport(df, title="Pandas Profiling Report")
+df2 = df.loc[(df['fldDate'] >= StartDate) & (df['fldDate'] < EndDate)]
+df2.head()
 
-df.head()
+plt.figure(figsize=(15, 5))
+sns.countplot(x='fldGroupTypeID', data=df2)
+plt.title(f'{StartDate} - {EndDate} furniture Catagories Count')
+plt.xlabel('Furniture Catagories Quoted')
 
-plt.bar('fldGroupTypeID', 'fldQuantity', label='Group Count')
-plt.title('Number of Installers vs Install Hours')
-plt.ylabel('Number of Hours')
-plt.xlabel('Number of Installers')
-plt.show()
+# Line Count of Parts by Type.
+plt.figure(figsize=(10, 30))
+sns.histplot(y='fldOpsWorksheetID', data=df2)
+plt.title(f'{StartDate} - {EndDate} Groups Count')
+plt.ylabel('Furniture System Catagories')
+plt.xlabel('count of entries')
